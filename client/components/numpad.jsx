2c1d75c37916d74828a3f76d6d3c24613128663c
Sang Tran <sang.tran1025@gmail.com>
2022-05-31 23:42:58 +0000

    inprogess of styling the numpad

--- client/components/numpad.jsx
+++ client/components/numpad.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 class NumPad extends React.Component {
   render() {
     return (
-      <div className="col-12 col-sm-12 col-lg-6">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
+      <div className="col-12 col-sm-12 col-lg-6 numpad">
+        <button className='num'>1</button>
+        <button className='num'>2</button>
+        <button className='num'>3</button>
+        <button className='num'>4</button>
+        <button className='num'>5</button>
+        <button className='num'>6</button>
+        <button className='num'>7</button>
+        <button className='num'>8</button>
+        <button className='num'>9</button>
       </div>
     );
   }
