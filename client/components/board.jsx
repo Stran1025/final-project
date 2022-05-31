6070c0681725180ea485f3034544f07cc05de82c
Sang Tran <sang.tran1025@gmail.com>
2022-05-31 20:19:41 +0000

    completed styling of the borders of the board

--- client/components/board.jsx
+++ client/components/board.jsx
@@ -5,7 +5,16 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      challenge: this.props.challenge
+      challenge: this.props.challenge,
+      layout: [['top left cell', 'top cell', 'top cell', 'top left cell', 'top cell', 'top cell', 'top left cell', 'top cell', 'top right cell'],
+        ['left cell', 'cell', 'cell', 'left cell', 'cell', 'cell', 'left cell', 'cell', 'right cell'],
+        ['left cell', 'cell', 'cell', 'left cell', 'cell', 'cell', 'left cell', 'cell', 'right cell'],
+        ['top left cell', 'top cell', 'top cell', 'top left cell', 'top cell', 'top cell', 'top left cell', 'top cell', 'top right cell'],
+        ['left cell', 'cell', 'cell', 'left cell', 'cell', 'cell', 'left cell', 'cell', 'right cell'],
+        ['left cell', 'cell', 'cell', 'left cell', 'cell', 'cell', 'left cell', 'cell', 'right cell'],
+        ['top left cell', 'top cell', 'top cell', 'top left cell', 'top cell', 'top cell', 'top left cell', 'top cell', 'top right cell'],
+        ['left cell', 'cell', 'cell', 'left cell', 'cell', 'cell', 'left cell', 'cell', 'right cell'],
+        ['bottom left cell', 'bottom cell', 'bottom cell', 'bottom left cell', 'bottom cell', 'bottom cell', 'bottom left cell', 'bottom cell', 'bottom right cell']]
     };
   }
 
@@ -17,7 +26,10 @@ class Board extends React.Component {
           return (
           <tr key={index} className='table-light'>
             {this.state.challenge[index].map((element, i) => {
-              return (<td key={i} className=''>{this.state.challenge[index][i]}</td>);
+              if (this.state.challenge[index][i]) {
+                return (<td key={i} className={this.state.layout[index][i]}>{this.state.challenge[index][i]}</td>);
+              }
+              return (<td key={i} className={this.state.layout[index][i]}></td>);
             })}
           </tr>
           );
