dd780f350dce9b88f267fe4a20c6e2b4d962d972
Sang Tran <sang.tran1025@gmail.com>
2022-05-31 18:35:59 +0000

    the table is now being load with actual data

--- client/app.jsx
+++ client/app.jsx
@@ -6,15 +6,23 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      sudoku: null
     };
   }
 
   componentDidMount() {
-
+    fetch('/api/sudoku')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ sudoku: data.challenge });
+      })
+      .catch(err => console.error('Error:', err));
   }
 
   render() {
-    return <Board />;
+    if (!this.state.sudoku) {
+      return (<h1>Hi</h1>);
+    }
+    return <Board challenge={this.state.sudoku} />;
   }
 }
