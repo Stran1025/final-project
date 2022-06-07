333714e9a7463919d53a246527a9ec8e2139a265
Sang Tran <sang.tran1025@gmail.com>
2022-06-07 19:45:21 +0000

    styling for the profile page is done

--- client/pages/profile.jsx
+++ client/pages/profile.jsx
@@ -3,23 +3,29 @@ import AppContext from '../lib/app-context';
 
 export default class Profile extends React.Component {
   render() {
+    const style = {
+      width: '35%'
+    };
     return (
       <div className="container">
         <div className="row">
-          <div className="col-12 card">
+          <div className="col-12 card half-height">
             <h1 className="text-center">{'Hi! ' + 'Sang!'}</h1>
-            <div className="d-flex">
-              <div className="border">
-                <h1>0</h1>
-                <p>Completed</p>
+            <div className="d-flex justify-content-evenly mt-3">
+              <div className="border info-box text-center d-flex align-items-center justify-content-center">
+                <p className='fs-4 fw-bold'>0<br></br><span className='fw-normal fs-6'>Completed</span></p>
               </div>
-              <div className="border">
-                <h1>Novice</h1>
-                <p>Title</p>
+              <div className="border info-box text-center d-flex align-items-center justify-content-center">
+                <p className='fs-4 fw-bold'>Novice<br></br><span className='fw-normal fs-6'>Title</span></p>
               </div>
-              <div className="border">
-                <h1>0</h1>
-                <p>Created</p>
+              <div className="border info-box text-center d-flex align-items-center justify-content-center">
+                <p className='fs-4 fw-bold'>0<br></br><span className='fw-normal fs-6'>Created</span></p>
+              </div>
+            </div>
+            <div className='mb-3'>
+              <p>{'Level: ' + '1'}</p>
+              <div className="progress">
+                <div className="progress-bar" style={style} aria-valuenow={0} aria-valuemin="0" aria-valuemax="100"></div>
               </div>
             </div>
           </div>
